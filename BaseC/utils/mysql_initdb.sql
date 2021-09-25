f10ab59e741ad04270db1738d0b995014ec5c22f
Andre_Sacilotti <andre.sacilotti@sflabs.com.br>
2021-09-25 16:39:51 -0300

    Feature: Elasticsearch syncronization with mysql database
    Here the elastic are used as a search engine and the mysql came to keep the data consistently stored

---
+++ BaseC/utils/mysql_initdb.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS financial_operations;
+CREATE TABLE financial_operations (
+  id BIGINT(50) UNSIGNED NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (id),
+  UNIQUE KEY unique_id (id),
+  cpf VARCHAR(11) NOT NULL,
+  operation_type ENUM("credit_card_purchase", "debit_card_purchase", "credit_bureau_consultation") NOT NULL,
+  realized_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  operation_value FLOAT,
+  modification_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  insertion_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+INSERT INTO financial_operations (
+    cpf, operation_type, operation_value
+    ) VALUES (
+    "12312312390", "credit_card_purchase", 1900.97
+    )
